3eff6d847c53eb988f16dbb2f3a0f795d47ae438
topcopy <34791343+topcopy@users.noreply.github.com>
2025-09-24 12:18:36 +0800

    Add files via upload

---
+++ x-ui-ray-alpine.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+ 
+cd /root
+apk update
+apk add curl
+curl -O -L https://github.com/XTLS/Xray-install/raw/main/alpinelinux/install-release.sh
+curl -Ls https://raw.githubusercontent.com/mhsanaei/3x-ui/master/install.sh
+ash -c ./install-release.sh
+bash -c ./install.sh
+
+
