78b6b67ca713a306162b90409e1865bb1216e791
topcopy <34791343+topcopy@users.noreply.github.com>
2025-09-25 09:54:49 +0800

    Update x-ui-ray-alpine.sh

--- x-ui-ray-alpine.sh
+++ x-ui-ray-alpine.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-curl -O -L https://github.com/XTLS/Xray-install/raw/main/alpinelinux/install-release.sh
-ash install-release.sh
-bash <(curl -Ls https://raw.githubusercontent.com/mhsanaei/3x-ui/master/install.sh)
+apk update && apk add curl bash && curl -O -L https://github.com/XTLS/Xray-install/raw/main/alpinelinux/install-release.sh && ash install-release.sh && bash <(curl -Ls https://raw.githubusercontent.com/mhsanaei/3x-ui/master/install.sh)
